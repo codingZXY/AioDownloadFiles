f1d241d6ace9a5a68c8db39da609937b24fe58b5
ethan <610310152@qq.com>
2019-09-27 18:41:23 +0800

    列表爬取

--- list_spider.py
+++ list_spider.py
@@ -1,9 +1,48 @@
 import requests
+import threading
 from openpyxl import Workbook
+from setting import *
 
 class ListSpider():
-    def __init__(self):
+    def __init__(self, tasks):
+        self.tasks = tasks
+
+    def crawl(self, url):
         pass
+
+    def clean(self, source):
+        pass
+
+    def save_rows(self, rows_data, ws):
+        pass
+
+    def save(self, wb):
+        pass
+
+    def run(self, task):
+        wb = Workbook()
+        ws = wb.active
+
+        aid = TASK_TYPE_ID[task]
+        page = 1
+        while 1:
+            url = f'http://www.c-whale.com/jsp/list?aid={aid}&bid=&row={page*10}'
+            source = self.crawl(url)
+            if source is None:
+                break
+
+            rows_data = self.clean(source)
+
+            self.save_rows(rows_data, ws)
+
+        self.save(wb)
+
+
+
+    def start(self):
+        pass
+
+
 
 
 
